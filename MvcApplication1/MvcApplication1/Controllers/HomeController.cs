6134d9c3af317725e51f61231afce80fb61f8495
muyake <1245570818@qq.com>
2014-09-09 22:13:00 +0800

    create

---
+++ MvcApplication1/MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "欢迎使用 ASP.NET MVC!";
+
+            return View();
+        }
+        public void test()
+        {
+            Response.Write("nihao");
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
